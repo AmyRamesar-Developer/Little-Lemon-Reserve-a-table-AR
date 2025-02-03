bfe2ae939707a7d0310c5ed5d799812fd7fa44da
Amy Ramesar <amy.ramesar@my.uwi.edu>
2025-02-03 12:32:38 -0400

    Simple styling updates and added Homepage component

--- littlelemonbooking/src/App.js
+++ littlelemonbooking/src/App.js
@@ -1,22 +1,10 @@
 import React from "react";
-import Header from './Header.js'
-import Hero from "./Hero.js"
-import SpecialSection from "./SpecialSection.js"
-import Testimonals from "./Testimonals.js"
-import About from "./About.js"
-import Footer from './Footer.js'
+import Homepage from "./Homepage"
 import './App.css'
 
 function App() {
   return (
-      <main className="grid-container">
-      <Header />
-      <Hero />
-      <SpecialSection />
-      <Testimonals />
-      <About />
-      <Footer />
-    </main>
+      <Homepage />
   );
 }
 
---
+++ littlelemonbooking/src/Homepage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from './Header.js'
+import Hero from "./Hero.js"
+import SpecialSection from "./SpecialSection.js"
+import Testimonals from "./Testimonals.js"
+import About from "./About.js"
+import Footer from './Footer.js'
+import './App.css'
+
+function Homepage() {
+  return (
+      <main className="grid-container">
+      <Header />
+      <Hero />
+      <SpecialSection />
+      <Testimonals />
+      <About />
+      <Footer />
+    </main>
+  );
+}
+
+export default Homepage;
